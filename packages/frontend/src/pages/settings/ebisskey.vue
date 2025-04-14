4db71a195a68b31208b4a2362bfe514cb7572016
Ebise <lutica@shrimpia.network>
2025-04-14 12:07:47 +0000

    独自機能ページを改良

--- packages/frontend/src/pages/settings/ebisskey.vue
+++ packages/frontend/src/pages/settings/ebisskey.vue
@@ -1,28 +1,34 @@
 <template>
 <SearchMarker path="/settings/ebisskey" label="独自機能" :keywords="['ebisskey']" icon="ti ti-power">
 	<div class="_gaps_m">
-		<h1>Ebisskey</h1>
+		<MkFeatureBanner icon="/client-assets/shrimp_3d.png" color="#b93e43">
+			<SearchKeyword>
+				本サーバー独自の機能を有効化・無効化できます。
+			</SearchKeyword>
+		</MkFeatureBanner>
 		<FormSection>
 			<template #label>独自機能</template>
 			<div class="_gaps_m">
-				<div>Ebisskeyが追加する独自機能を有効・無効にします。</div>
-
 				<SearchMarker :keywords="['nickname']">
-					<MkSwitch v-model="nicknameEnabled">
-						<SearchLabel>ニックネーム機能</SearchLabel>
-						<template #caption>
-							ユーザーページにて、ユーザーの名前をクリック/タップすることで好きなものに変更できるようになります。変更は自分にのみ反映されます。<br>
-							頻繁に名前を変更するユーザーを識別するときなどに使えます。
-						</template>
-					</MkSwitch>
+					<MkPreferenceContainer k="ebisskey.nicknameEnabled">
+						<MkSwitch v-model="nicknameEnabled">
+							<SearchLabel>ニックネーム機能</SearchLabel>
+							<template #caption>
+								ユーザーページにて、ユーザーの名前をクリック/タップすることで好きなものに変更できるようになります。変更は自分にのみ反映されます。<br>
+								頻繁に名前を変更するユーザーを識別するときなどに使えます。
+							</template>
+						</MkSwitch>
+					</MkPreferenceContainer>
 				</SearchMarker>
 				<SearchMarker :keywords="['steal']">
-					<MkSwitch v-model="stealEnabled">
-						<SearchLabel>パクる機能</SearchLabel>
-						<template #caption>
-							ノートをコピーしてそのまま投稿する機能。
-						</template>
-					</MkSwitch>
+					<MkPreferenceContainer k="ebisskey.stealEnabled">
+						<MkSwitch v-model="stealEnabled">
+							<SearchLabel>パクる機能</SearchLabel>
+							<template #caption>
+								ノートをコピーしてそのまま投稿する機能。
+							</template>
+						</MkSwitch>
+					</MkPreferenceContainer>
 				</SearchMarker>
 			</div>
 		</FormSection>
@@ -32,19 +38,25 @@
 				<div>Misskeyの機能に変更を加えます。</div>
 
 				<SearchMarker :keywords="['note', 'info', 'button']">
-					<MkSwitch v-model="infoButtonForNoteActionsEnabled">
-						<SearchLabel>ノートに詳細表示ボタンを表示する</SearchLabel>
-						<template #caption>
-							オプション「ノートの操作部をホバー時のみ表示する」をオンにしたときに適用されます。
-						</template>
-					</MkSwitch>
+					<MkPreferenceContainer k="ebisskey.infoButtonForNoteActionsEnabled">
+						<MkSwitch v-model="infoButtonForNoteActionsEnabled">
+							<SearchLabel>ノートに詳細表示ボタンを表示する</SearchLabel>
+							<template #caption>
+								オプション「{{ i18n.ts.showNoteActionsOnlyHover }}」をオンにしたときに適用されます。
+							</template>
+						</MkSwitch>
+					</MkPreferenceContainer>
 				</SearchMarker>
-				<MkSwitch v-model="rememberPostFormToggleStateEnabled">
-					<SearchLabel>投稿フォームにて、プレビューのオン・オフを記憶する</SearchLabel>
-				</MkSwitch>
-				<MkSwitch v-model="featuredTimelineEnabled">
-					<SearchLabel>ハイライトタイムラインをホームに表示する</SearchLabel>
-				</MkSwitch>
+				<MkPreferenceContainer k="ebisskey.rememberPostFormToggleStateEnabled">
+					<MkSwitch v-model="rememberPostFormToggleStateEnabled">
+						<SearchLabel>投稿フォームにて、プレビューのオン・オフを記憶する</SearchLabel>
+					</MkSwitch>
+				</MkPreferenceContainer>
+				<MkPreferenceContainer k="ebisskey.featuredTimelineEnabled">
+					<MkSwitch v-model="featuredTimelineEnabled">
+						<SearchLabel>ハイライトタイムラインをホームに表示する</SearchLabel>
+					</MkSwitch>
+				</MkPreferenceContainer>
 			</div>
 		</FormSection>
 		<FormSection>
@@ -53,50 +65,74 @@
 				<div>まだ開発中の機能を試してみませんか。一部の機能はちゃんと動かないかもしれません。</div>
 
 				<!-- エアリプ機能 -->
-				<div class="_panel _padding _gaps_m">
-					<SearchMarker :keywords="['air-reply']">
-						<MkSwitch v-model="useAirReply">
-							<SearchLabel>エアリプ機能</SearchLabel>
-							<template #caption>
-								特定のノートに曖昧な返信を行うための、公開範囲を合わせた投稿ボタンを有効化します。
-							</template>
-						</MkSwitch>
-					</SearchMarker>
-					<MkSelect v-if="useAirReply" v-model="airReplyButtonPlacement">
-						<template #label>ボタンの位置</template>
-						<option value="noteMenu">ノートのメニュー</option>
-						<option value="renoteMenu">リノートボタンのメニュー</option>
-						<option value="noteFooter">ノートの下部</option>
-					</MkSelect>
-				</div>
+				<SearchMarker :keywords="['air-reply']">
+					<MkFolder :defaultOpen="useAirReply">
+						<template #icon><i class="ti ti-bubble-text"></i></template>
+						<template #label><SearchLabel>エアリプ機能</SearchLabel></template>
+						<div class="_gaps_m">
+							<div>
+								ノートに、公開範囲を合わせた投稿フォームを開くボタンを追加します。<br/>
+								特定のノートに曖昧な返信を行うときに便利です。
+							</div>
+							<MkPreferenceContainer k="ebisskey.useAirReply">
+								<MkSwitch v-model="useAirReply">
+									<SearchLabel>オンにする</SearchLabel>
+								</MkSwitch>
+							</MkPreferenceContainer>
+							<MkPreferenceContainer k="ebisskey.airReplyButtonPlacement">
+								<MkSelect v-model="airReplyButtonPlacement">
+									<template #label>ボタンの位置</template>
+									<option value="noteMenu">ノートのメニュー</option>
+									<option value="renoteMenu">リノートボタンのメニュー</option>
+									<option value="noteFooter">ノートの下部</option>
+								</MkSelect>
+							</MkPreferenceContainer>
+						</div>
+					</MkFolder>
+				</SearchMarker>
 
-				<div class="_panel _padding _gaps_m">
-					<SearchMarker :keywords="['note', 'visibility', 'coloring']">
-						<MkSwitch v-model="useNoteVisibilityColoring">
-							<SearchLabel>公開範囲に応じた色分け</SearchLabel>
-							<template #caption>
-								ノートの公開範囲に応じて、特殊な色付き表示を行います。
-							</template>
-						</MkSwitch>
-					</SearchMarker>
-					<template v-if="useNoteVisibilityColoring">
-						<MkColorInput v-model="noteVisibilityColorHome">
-							<template #label>{{ i18n.ts._visibility.home }}</template>
-						</MkColorInput>
-						<MkColorInput v-model="noteVisibilityColorFollowers">
-							<template #label>{{ i18n.ts._visibility.followers }}</template>
-						</MkColorInput>
-						<MkColorInput v-model="noteVisibilityColorSpecified">
-							<template #label>{{ i18n.ts._visibility.specified }}</template>
-						</MkColorInput>
-						<MkColorInput v-model="noteVisibilityColorLocalOnly">
-							<template #label>{{ i18n.ts._visibility.public }}（{{ i18n.ts._visibility.disableFederation }}）</template>
-						</MkColorInput>
-						<MkButton v-if="noteVisibilityColorChanged" primary @click="saveColors"><i class="ti ti-device-floppy"></i> {{ i18n.ts.save }}</MkButton>
-					</template>
-				</div>
+				<!-- ノートの公開範囲に応じた色分け -->
+				<SearchMarker :keywords="['note', 'visibility', 'coloring']">
+					<MkFolder :defaultOpen="useNoteVisibilityColoring">
+						<template #icon><i class="ti ti-color-swatch"></i></template>
+						<template #label><SearchLabel>ノートの公開範囲に応じた色分け</SearchLabel></template>
+						<div class="_gaps_m">
+							<div>
+								ノートの公開範囲に応じて、色付き表示を行います。<br/>
+								ノートの公開範囲をより視覚的に区別することができます。
+							</div>
+							<MkPreferenceContainer k="ebisskey.useNoteVisibilityColoring">
+								<MkSwitch v-model="useNoteVisibilityColoring">
+									<SearchLabel>オンにする</SearchLabel>
+								</MkSwitch>
+							</MkPreferenceContainer>
 
-				<div class="_panel _padding _gaps_m">
+							<MkColorInput v-model="noteVisibilityColorHome">
+								<template #label>{{ i18n.ts._visibility.home }}</template>
+							</MkColorInput>
+							<MkColorInput v-model="noteVisibilityColorFollowers">
+								<template #label>{{ i18n.ts._visibility.followers }}</template>
+							</MkColorInput>
+							<MkColorInput v-model="noteVisibilityColorSpecified">
+								<template #label>{{ i18n.ts._visibility.specified }}</template>
+							</MkColorInput>
+							<MkColorInput v-model="noteVisibilityColorLocalOnly">
+								<template #label>{{ i18n.ts._visibility.public }}（{{ i18n.ts._visibility.disableFederation }}）</template>
+							</MkColorInput>
+						</div>
+						<template #footer>
+							<div class="_buttons">
+								<MkButton :disabled="!noteVisibilityColorChanged" primary @click="saveColors">
+									<i class="ti ti-device-floppy"></i> {{ i18n.ts.save }}
+								</MkButton>
+							</div>
+						</template>
+					</MkFolder>
+				</SearchMarker>
+
+				<MkFolder :defaultOpen="true">
+					<template #icon><i class="ti ti-settings"></i></template>
+					<template #label>その他</template>
 					<SearchMarker :keywords="['textarea', 'auto', 'size']">
 						<MkSwitch v-model="useTextAreaAutoSize">
 							<SearchLabel>テキストエリアの自動サイズ調整</SearchLabel>
@@ -106,12 +142,7 @@
 							</template>
 						</MkSwitch>
 					</SearchMarker>
-				</div>
-
-				<!-- 不具合で動かないため一度オフ -->
-				<!-- <MkSwitch v-model="usePostFormWindow">
-					投稿フォームをウィンドウとして表示
-				</MkSwitch> -->
+				</MkFolder>
 			</div>
 		</FormSection>
 	</div>
@@ -120,6 +151,7 @@
 
 <script lang="ts" setup>
 import { computed, ref, watch } from 'vue';
+import type { Note } from 'misskey-js/entities.js';
 import MkSwitch from '@/components/MkSwitch.vue';
 import FormSection from '@/components/form/section.vue';
 import { i18n } from '@/i18n';
@@ -129,6 +161,12 @@ import MkColorInput from '@/components/MkColorInput.vue';
 import MkButton from '@/components/MkButton.vue';
 import { prefer } from '@/preferences';
 import { definePage } from '@/page';
+import MkFeatureBanner from '@/components/MkFeatureBanner.vue';
+import MkPreferenceContainer from '@/components/MkPreferenceContainer.vue';
+import MkFolder from '@/components/MkFolder.vue';
+import { PREF_DEF } from '@/preferences/def';
+import { $i } from '@/i';
+import MkNote from '@/components/MkNote.vue';
 
 const nicknameEnabled = prefer.model('ebisskey.nicknameEnabled');
 const stealEnabled = prefer.model('ebisskey.stealEnabled');
@@ -145,6 +183,25 @@ const noteVisibilityColorFollowers = ref(prefer.s['ebisskey.noteVisibilityColorF
 const noteVisibilityColorSpecified = ref(prefer.s['ebisskey.noteVisibilityColorSpecified']);
 const noteVisibilityColorLocalOnly = ref(prefer.s['ebisskey.noteVisibilityColorLocalOnly']);
 const noteVisibilityColorChanged = ref(false);
+
+const noteMockupVisibility = ref<'public' | 'home' | 'followers' | 'specified'>('home');
+const noteMockupLocalOnly = ref(false);
+
+const noteMockup = computed(() => $i ? ({
+	id: '0',
+	createdAt: new Date().toISOString(),
+	text: 'こんにちは',
+	userId: $i.id,
+	user: $i,
+	visibility: noteMockupVisibility.value,
+	localOnly: noteMockupLocalOnly.value,
+	reactionAcceptance: null,
+	reactionEmojis: {},
+	reactions: {},
+	reactionCount: 0,
+	renoteCount: 0,
+	repliesCount: 0,
+} satisfies Note) : null);
 
 watch([
 	noteVisibilityColorHome,
@@ -167,13 +224,20 @@ watch([
 });
 
 function saveColors() {
-	if (noteVisibilityColorChanged.value) {
-		prefer.commit('ebisskey.noteVisibilityColorHome', noteVisibilityColorHome.value);
-		prefer.commit('ebisskey.noteVisibilityColorFollowers', noteVisibilityColorFollowers.value);
-		prefer.commit('ebisskey.noteVisibilityColorSpecified', noteVisibilityColorSpecified.value);
-		prefer.commit('ebisskey.noteVisibilityColorLocalOnly', noteVisibilityColorLocalOnly.value);
-		noteVisibilityColorChanged.value = false;
-	}
+	if (!noteVisibilityColorChanged.value) return;
+
+	prefer.commit('ebisskey.noteVisibilityColorHome', noteVisibilityColorHome.value);
+	prefer.commit('ebisskey.noteVisibilityColorFollowers', noteVisibilityColorFollowers.value);
+	prefer.commit('ebisskey.noteVisibilityColorSpecified', noteVisibilityColorSpecified.value);
+	prefer.commit('ebisskey.noteVisibilityColorLocalOnly', noteVisibilityColorLocalOnly.value);
+	noteVisibilityColorChanged.value = false;
+}
+
+function resetColors() {
+	noteVisibilityColorHome.value = PREF_DEF['ebisskey.noteVisibilityColorHome'].default;
+	noteVisibilityColorFollowers.value = PREF_DEF['ebisskey.noteVisibilityColorFollowers'].default;
+	noteVisibilityColorSpecified.value = PREF_DEF['ebisskey.noteVisibilityColorSpecified'].default;
+	noteVisibilityColorLocalOnly.value = PREF_DEF['ebisskey.noteVisibilityColorLocalOnly'].default;
 }
 
 const headerActions = computed(() => []);
